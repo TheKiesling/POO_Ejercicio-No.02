1bb741b9e4031ce902737a31801ea3a1307c45c8
TheKiesling <kie21581@uvg.edu.gt>
2021-08-17 21:09:56 -0600

    Create Perro.java
    Clase que crea perros con todos sus datos

---
+++ Perro.java
@@ -0,0 +1,68 @@
+/*
+#      #    #######    ########   #######   #          #######   ##      #    #########
+#     #        #       #          #         #             #      # #     #    #
+#    #         #       #          #         #             #      #  #    #    #
+####           #       #####      #######   #             #      #   #   #    #    ####
+#    #         #       #                #   #             #      #    #  #    #       #
+#     #        #       #                #   #             #      #     # #    #       #
+#      #    ########   ########   #######   ########   #######   #      ##    #########
+*/
+
+public class Perro {
+    /** 
+    @author: José Pablo Kiesling Lange
+    Nombre del programa: Perro.java
+    @version: 
+        - Creación: 17/08/2021
+        - Última modificación: 17/08/2021
+
+    Clase que crea perros con todos sus datos
+    */
+
+    //---------------------------PROPIEDADES-------------------------
+    private String tamaño;
+    private String raza;
+    private int edad;
+    private int estado_salud;
+    private String color;
+    private String nombre;
+
+    //---------------------------MÉTODOS-----------------------------
+
+    /****************************************************************
+     * Constructor: asigna los datos al perro instanciado
+     * @param tamaño
+     * @param raza
+     * @param edad
+     * @param estado_salud
+     * @param color
+     * @param nombre
+     */
+    public Perro(String tamaño, String raza, int edad, int estado_salud, String color, String nombre){
+        this.tamaño = tamaño;
+        this.raza = raza;
+        this.edad = edad;
+        this.estado_salud = estado_salud;
+        this.color = color;
+        this.nombre = nombre;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * getterTamaño: retorna el valor del tamaño
+     * @return
+     */
+    private String getTamaño(){
+        return tamaño;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * getterRaza: retorna el valor de la raza
+     * @return
+     */
+    private String raza(){
+        return raza;
+    }
+    //***************************************************************
+}
